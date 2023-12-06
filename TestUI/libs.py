dcffdf16b68a097570fc255f22f6c391278f0f35
HaruHoo <huchenyifan@126.com>
2023-12-06 13:10:27 +0800

    The final visual interface of the Sender and Receiver

---
+++ TestUI/libs.py
@@ -0,0 +1,13 @@
+import socket
+import cv2
+import numpy
+import time
+import sys
+import os
+import struct
+import matplotlib.pyplot as plt
+import matplotlib.image as image
+from io import BytesIO
+from PIL import ImageGrab
+import tkinter
+from PyQt5.QtCore import QTimer
